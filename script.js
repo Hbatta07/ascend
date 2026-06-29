af26f8a2b1195b127f335babb0aed61835a794af
Hbatta07 <himalayabatta@gmail.com>
2026-06-30 04:52:32 +0530

    Animate initial XP progress

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 // =========================================
-// ASCEND v0.1
-// User Onboarding
+// ASCEND v0.2
+// User + XP
 // =========================================
 
 const saveUser = document.getElementById("saveUser");
 const usernameInput = document.getElementById("usernameInput");
 const greeting = document.getElementById("greeting");
 const onboarding = document.getElementById("onboarding");
+const progressBar = document.querySelector(".progress-fill");
 
 loadUser();
 
 saveUser.addEventListener("click", beginJourney);
 
-function beginJourney() {
+function beginJourney(){
 
     const username = usernameInput.value.trim();
 
-    if (username === "") return;
+    if(username === "") return;
 
     localStorage.setItem("username", username);
 
@@ -24,22 +25,36 @@ function beginJourney() {
 
 }
 
-function loadUser() {
+function loadUser(){
 
     const username = localStorage.getItem("username");
 
-    if (username) {
+    if(username){
 
         greeting.innerHTML = `Welcome back,<br>${username}.`;
 
         onboarding.style.display = "none";
 
-    } else {
+    }else{
 
         greeting.textContent = "Welcome.";
 
         onboarding.style.display = "block";
 
     }
+
+    updateXP();
+
+}
+
+function updateXP(){
+
+    const xp = 120;
+
+    const maxXP = 1000;
+
+    const percentage = (xp / maxXP) * 100;
+
+    progressBar.style.width = percentage + "%";
 
 }
