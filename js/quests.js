ddff997aa3155384302114343538969eb441efda
Hbatta07 <himalayabatta@gmail.com>
2026-06-30 10:51:07 +0530

    Create quest data file

---
+++ js/quests.js
@@ -0,0 +1,18 @@
+const quests = [
+
+    {
+        title: "💧 Drink 2L Water",
+        xp: 25
+    },
+
+    {
+        title: "📖 Read 10 Pages",
+        xp: 40
+    },
+
+    {
+        title: "🚶 Walk 30 Minutes",
+        xp: 35
+    }
+
+];
